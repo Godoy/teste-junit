ec36d209021690c38e0b224785575941aac297b2
Adriano Godoy <godoy.ccp@gmail.com>
2017-07-01 09:15:47 -0300

    Setup junit3

---
+++ src/br/net/godoy/MainTest.java
@@ -0,0 +1,15 @@
+package br.net.godoy;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by adrianogodoy on 7/1/17.
+ */
+public class MainTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testMain() throws Exception {
+    }
+}
